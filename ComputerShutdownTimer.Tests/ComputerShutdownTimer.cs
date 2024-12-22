5e9af0462d55960a94e02cc1228b30decca59bea
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2024-12-22 23:33:20 +0200

    Add project files.

---
+++ ComputerShutdownTimer.Tests/ComputerShutdownTimer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputerShutdownTimer.Tests
+{
+    [TestClass]
+    public class ComputerShutdownTimer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ComputerShutdownTimer.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ComputerShutdownTimer.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ComputerShutdownTimer.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d58cc41-4dec-4ea2-bc39-af83e69e8889")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
