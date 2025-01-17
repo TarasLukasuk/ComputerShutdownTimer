f472669bdf14290f65e41a6ff101654687e92b58
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2025-01-17 19:35:29 +0200

    Refactor window resizing logic
    - Moved `DirectionStretching` enum inside `WindowResizer` for better encapsulation.
    - Added `ResizeHandlers` dictionary to simplify window resize handler mapping.
    - Updated XAML to include named rectangles with corresponding resize directions.
    - Implemented `Resize_MouseLeftButtonDown` in `MainWindow` to delegate resizing to `WindowResizer`.

--- ComputerShutdownTimer/Services/WindowResizer .cs
+++ ComputerShutdownTimer/Services/WindowResizer .cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
@@ -6,18 +7,6 @@ using System.Windows.Interop;
 
 namespace ComputerShutdownTimer.Services
 {
-    internal enum DirectionStretching
-    {
-        Left = 1,
-        Right = 2,
-        Top = 3,
-        TopLeft = 4,
-        TopRight = 5,
-        Bottom = 6,
-        BottomLeft = 7,
-        BottomRight = 8
-    }
-
     internal class WindowResizer
     {
         private const int WM_SYSCOMMAND = 0x112;
@@ -25,21 +14,47 @@ namespace ComputerShutdownTimer.Services
 
         private readonly Window _window;
 
+        private enum DirectionStretching
+        {
+            Left = 1,
+            Right = 2,
+            Top = 3,
+            TopLeft = 4,
+            TopRight = 5,
+            Bottom = 6,
+            BottomLeft = 7,
+            BottomRight = 8
+        }
+
+        internal Dictionary<string, Action> ResizeHandlers { get; }
+
         [DllImport("user32.dll")]
         private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
         public WindowResizer(Window window)
         {
             _window = window ?? throw new ArgumentNullException(nameof(window));
+
+            ResizeHandlers = new Dictionary<string, Action>
+            {
+                { "ResizeTop",  () => ResizeWindow(DirectionStretching.Top, Cursors.SizeNS) },
+                { "ResizeBottom", () => ResizeWindow(DirectionStretching.Bottom, Cursors.SizeNS) },
+                { "ResizeLeft", () => ResizeWindow(DirectionStretching.Left, Cursors.SizeWE) },
+                { "ResizeRight", () => ResizeWindow(DirectionStretching.Right, Cursors.SizeWE) },
+                { "ResizeTopLeft", () => ResizeWindow(DirectionStretching.TopLeft, Cursors.SizeNWSE) },
+                { "ResizeBottomRight", () => ResizeWindow(DirectionStretching.BottomRight, Cursors.SizeNWSE) },
+                { "ResizeTopRight", () => ResizeWindow(DirectionStretching.TopRight, Cursors.SizeNESW) },
+                { "ResizeBottomLeft", () => ResizeWindow(DirectionStretching.BottomLeft, Cursors.SizeNESW) }
+            };
         }
 
-        public void ResizeWindow(DirectionStretching directionStretching, Cursor cursor)
+        private void ResizeWindow(DirectionStretching directionStretching, Cursor cursor)
         {
             try
             {
                 Mouse.OverrideCursor = cursor;
 
-                SendMessage(new WindowInteropHelper(_window).Handle, WM_SYSCOMMAND, (IntPtr)(SC_SIZE + (int)directionStretching), IntPtr.Zero);
+                SendMessage(new WindowInteropHelper(_window).Handle, WM_SYSCOMMAND, (IntPtr)(SC_SIZE + directionStretching), IntPtr.Zero);
             }
             finally
             {
--- ComputerShutdownTimer/Views/Forms/MainWindow.xaml.cs
+++ ComputerShutdownTimer/Views/Forms/MainWindow.xaml.cs
@@ -1,12 +1,31 @@
-﻿using System.Windows;
+﻿using ComputerShutdownTimer.Services;
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace ComputerShutdownTimer.Views.Forms
 {
-    partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
+        private readonly WindowResizer _windowResizer;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _windowResizer = new WindowResizer(this);
+        }
+
+        private void Resize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Rectangle rectangle)
+            {
+                if (_windowResizer.ResizeHandlers.TryGetValue(rectangle.Name, out Action action))
+                {
+                    action.Invoke();
+                }
+            }
         }
     }
 }
