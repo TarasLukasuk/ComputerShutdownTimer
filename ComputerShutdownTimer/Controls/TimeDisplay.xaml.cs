ae649ef70a95f31553efb6f46fd817d2278f68c4
TarasLukasuk <121807502+TarasLukasuk@users.noreply.github.com>
2025-03-07 16:14:29 +0200

    refactor: updated main window and added new TimeDisplay component
    - **Main window updates:**
      - Removed unnecessary `MinHeight` and `MinWidth` properties as they were unused.
      - Added rotation animation for the arrow in `RoundCheckBox` when the `IsChecked` state changes.
      - Adjusted the size and positioning of elements in the top panel for better visual appearance.
      - Added `Panel.ZIndex="1"` to the top border to prevent element overlap.
      - Updated the grid structure in the main container for better element placement.
      - Added a new `Grid` for displaying time and a button in the main container.
    
    - **Added new TimeDisplay component:**
      - Created a `UserControl` to display time in the format "00:00:00".
      - Used `Viewbox` for automatic scaling of content based on container size.
      - Used a horizontally oriented `StackPanel` to arrange time text and separators.
      - Added `TimeTextStyle` and `SeparatorStyle` styles for time text and colon separators.
    
    These changes improve the interface structure, add new functionality for time display, and enhance user interaction.

---
+++ ComputerShutdownTimer/Controls/TimeDisplay.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ComputerShutdownTimer.Controls
+{
+    public partial class TimeDisplay : UserControl
+    {
+        public TimeDisplay()
+        {
+            InitializeComponent();
+        }
+    }
+}
